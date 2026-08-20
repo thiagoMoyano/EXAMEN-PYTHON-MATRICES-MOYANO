3740538abdab81f4c98d6872a1d733bb74100ea3
Thiago Moyano <td.moyano@alumno.etec.um.edu.ar>
2026-08-20 10:29:10 -0300

    Lo hize

--- ejercicio1/ej1.py
+++ ejercicio1/ej1.py
@@ -1,9 +1,9 @@
 
-fila1 = ["A", "B", "C"]
-fila2 = ["D", "E", "F"]
-fila3 = ["G", "H", "I"]
-
-matriz = [fila1, fila2, fila3]
+matriz = [
+    ["A", "B", "C"],
+    ["D", "E", "F"],
+    ["G", "H", "I"]
+]
 
 for i in range(len(matriz)):
     print(f"Fila{i + 1:2d}: {matriz[i]}")
